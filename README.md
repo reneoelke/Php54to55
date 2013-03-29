666a35a2f72c59223e109fe8f4d512f1ab278e94
Marcel Eichner <love@ephigenia.de>
2013-03-29 21:59:05 +0100

    added sniff which checks for newly added global class names

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ This is a collection of sniffs for [PHP_CodeSniffer](http://pear.php.net/PHP_Cod
 * Check for removed Constants (MySQL)
 * Check for `setTimeZoneID` or `datefmt_set_timezone_id` usage
 * Check for definitions of global functions which are [new in PHP 5.5](http://www.php.net/manual/en/migration55.new-functions.php)
+* Check for definitions of classes that have been added in PHP 5.5
 
 # In Progress
 
@@ -18,6 +19,7 @@ This is a collection of sniffs for [PHP_CodeSniffer](http://pear.php.net/PHP_Cod
 * Check for usage of [deprecated MySQL constants](http://www.php.net/manual/en/mysql.constants.php: `MYSQL_CLIENT_COMPRESS`, `MYSQL_CLIENT_IGNORE_SPACE`, `MYSQL_CLIENT_INTERACTIVE`, `MYSQL_SSL`, `MYSQL_ASSOC`, `MYSQL_BOTH`, `MYSQL_NUM`
 * Check for calls to `setTimeZoneID()` of objects or procedural style `datefmt_set_timezone_id`
 * Check for a definition of a global function which was [added in PHP 5.5](http://www.php.net/manual/en/migration55.new-functions.php)
+* Check for definitions of [classes that have been added in PHP 5.5](http://www.php.net/manual/en/migration55.classes.php)
 
 # Usage
 
---
+++ Sniffs/PHP/ForbiddenClassNamesSniff.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Forbidden Function Names
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
+ * @copyright 2012 foobugs oelke & eichner GbR
+ * @license   BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link      https://github.com/foobugs/PHP53to54
+ * @since     1.0-beta
+ */
+
+/**
+ * Forbidden Class names
+ *
+ * Searches for definitions of global classes which were added in PHP 5.4.
+ * A complete list: http://www.php.net/manual/en/migration55.classes.php
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Marcel Eichner // foobugs <marcel.eichner@foobugs.com>
+ * @copyright 2012 foobugs oelke & eichner GbR
+ * @license   BSD http://www.opensource.org/licenses/bsd-license.php
+ * @link      https://github.com/foobugs/PHP53to54
+ * @since     1.0-beta
+ */
+class PHP54to55_Sniffs_PHP_ForbiddenClassNamesSniff
+extends PHP54to55_AbstractSniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+        'PHP',
+    );
+
+    /** @inheritdoc */
+    public function register()
+    {
+        return array(
+            T_CLASS,
+        );
+    }
+
+    /**
+     * A list of forbidden function names
+     *
+     * @var array(string => array(string, [string]))
+     */
+    protected $forbiddenClassnames = array(
+        'IntlCalendar',
+        'IntlGregorianCalendar',
+        'IntlTimeZone',
+        'IntlBreakIterator',
+        'IntlRuleBasedBreakIterator',
+        'IntlCodePointBreakIterator',
+    );
+    
+    /** @inheritdoc */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $token = $tokens[$stackPtr];
+
+        // find the name of the defined class
+        $nameOfClassStackPtr = $phpcsFile->findNext(array(T_STRING), $stackPtr, null, false);
+        if (!$nameOfClassStackPtr) {
+            return true;
+        }
+        $nameOfClassToken = $tokens[$nameOfClassStackPtr];
+        $nameOfClass = $nameOfClassToken['content'];
+
+        // check if the class name is forbidden
+        $nameOfClass = strtolower($nameOfClass);
+        foreach($this->forbiddenClassnames as $forbiddenClassname) {
+
+            if (strtolower($forbiddenClassname) == $nameOfClass) {
+                $message = sprintf(
+                    '%s was added in PHP 5.5 the global namespace and can’t be defined',
+                    $forbiddenClassname
+                );
+                $phpcsFile->addError(
+                    $message, $stackPtr
+                );
+            }
+        }
+
+        return true;
+    }   
+}
