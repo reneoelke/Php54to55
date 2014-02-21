3dc2bb411b603e7b3a2c9010cb2f11e02d25f5e9
goatherd <maik@phpkuh.de>
2014-02-21 23:53:52 +0100

    fix quirk

--- tests/Sniffs/PHP/ForbiddenClassNamesTest.php
+++ tests/Sniffs/PHP/ForbiddenClassNamesTest.php
@@ -12,14 +12,24 @@ class ForbiddenClassNamesTest extends \AbstractPhpcsTestCase
     protected $sniffs = array('Php54to55.PHP.ForbiddenClassNames');
     protected $defaultType = "Php54to55.PHP.ForbiddenClassNames";
 
-    protected $errors = array('13:1', '14:1', '15:1', '18:1', '19:1', '20:1', '21:1', '22:1', '23:1');
+    protected $errors = array(
+        '13:1',
+        '14:1',
+        '15:1', // PHP 5.4+ only
+        '18:1',
+        '19:1',
+        '20:1',
+        '21:1',
+        '22:1',
+        '23:1',
+    );
 
     /** {@inheritdoc} */
     public function fixtureSniffProvider()
     {
         if (version_compare(PHP_VERSION, '5.4.0', '<')) {
             // can not test traits with php 5.3
-            unset($this->errors['15:1']);
+            unset($this->errors[2]);
         }
 
         $this->fixture = __DIR__ . '/_fixtures/forbiddenClassNames/1.inc';
