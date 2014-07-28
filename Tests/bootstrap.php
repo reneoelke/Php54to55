c389b4bb1a1de787a443fd90d5c114de4f3cf867
René Oelke <rene.oelke@gmail.com>
2014-07-28 11:23:33 +0200

    Added bootstrap file for unit tests.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,12 +1,4 @@
 <?php
 
-// require abstract test case
-require_once __DIR__ . '/AbstractPhpcsTestCase.php';
-
-if (file_exists(__DIR__ . '/../vendor/bin/phpcs')) {
-    // default to composer installed phpcs
-    AbstractPhpcsTestCase::$phpcsBinary = __DIR__ . '/../vendor/bin/phpcs';
-} else {
-    // fall back to system phpcs (*nix only)
-    AbstractPhpcsTestCase::$phpcsBinary = trim(`which phpcs 2>/dev/null`);
-}
+require __DIR__ . '/../Sniffs/bootstrap.php';
+require __DIR__ . '/AbstractTestCase.php';
