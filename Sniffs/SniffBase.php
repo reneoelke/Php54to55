7a1fc25546ca17c0e371935f7050b55b751ca155
goatherd <maik@phpkuh.de>
2014-07-29 17:53:05 +0200

    fix psr2 compatibility

--- Sniffs/SniffBase.php
+++ Sniffs/SniffBase.php
@@ -9,38 +9,38 @@ use PHP_CodeSniffer_Sniff;
  */
 abstract class SniffBase implements PHP_CodeSniffer_Sniff
 {
-        /**
-         * A list of tokenizers this sniff supports.
-         *
-         * @var array
-         */
-        public $supportedTokenizers = array(
-            'PHP',
-        );
-    
-        /**
-         * Tokens to register for.
-         *
-         * @var array
-        */
-        protected $fooRegisterTokens = array();
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+        'PHP',
+    );
 
-        /**
-         * Helpful method with proper scope.
-         *
-         * @var Helper
-         */
-        protected $foo;
+    /**
+     * Tokens to register for.
+     *
+     * @var array
+    */
+    protected $fooRegisterTokens = array();
 
-        /**
-         * {@inherited}
-         */
-        public function register()
-        {
-            if (!isset($this->foo)) {
-                $this->foo = new Helper();
-            }
+    /**
+     * Helpful method with proper scope.
+     *
+     * @var Helper
+     */
+    protected $foo;
 
-            return $this->fooRegisterTokens;
+    /**
+     * {@inherited}
+     */
+    public function register()
+    {
+        if (!isset($this->foo)) {
+            $this->foo = new Helper();
         }
+
+        return $this->fooRegisterTokens;
+    }
 }
